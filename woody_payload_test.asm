978aa053cb33372ce7affd1d2fa475209d2e1d9b
Sithi5 <ma.sithis@gmail.com>
2021-07-30 14:12:16 +0200

    update asm test payload

--- woody_payload_test.asm
+++ woody_payload_test.asm
@@ -1,15 +1,17 @@
- section .data
-        WoodyMsg:     db '....WOODY....\n',14
-        WoodyMsgSize:    equ $-WoodyMsg
- section .text
-        global _start
- _start:
-        mov eax,4             ; Appel système "write" (sys_write)
-        mov ebx,1             ; File descriptor, 1 pour STDOUT (sortie standard)
-        mov ecx, WoodyMsg     ; Adresse de la chaîne a afficher
-        mov edx, WoodyMsgSize ; Taille de la chaîne
-        int 80h               ; Exécution de l'appel système
-                              ; Sortie du programme
-        mov eax,1             ; Appel système "exit"
-        mov ebx,0             ; Code de retour
-        int 80h
+section .data
+    WoodyMsg:     db '....WOODY....',10,0
+    WoodyMsgSize:    equ $-WoodyMsg
+    format: db '%s',0
+section .text
+    extern printf
+
+    global main
+
+main:
+    push rbp
+    mov rbp,rsp
+    mov rdi, format
+    mov rsi, WoodyMsg
+    call printf
+    leave
+    ret                     ; Sortie du programme
