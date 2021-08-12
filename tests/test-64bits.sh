2d0c5971e0bacca7faedf18390495e313ed2a4d4
Sithi5 <ma.sithis@gmail.com>
2021-08-12 13:23:04 +0200

    update tests

--- tests/test-64bits.sh
+++ tests/test-64bits.sh
@@ -3,7 +3,7 @@ _YELLOW=`tput setaf 3`
 _END=`tput sgr0`
 
 # echo filename
-echo "\n\n${_YELLOW}$0 :${_END}\n"
+echo "\n\n\n\n${_YELLOW}$0 :${_END}\n\n"
 
 
 # Test with two simples binary
@@ -12,12 +12,14 @@ echo "\n${_GREEN}Creating and Executing woody with no_pie binary...${_END}\n"
 ./woody_woodpacker test1_no_pie
 ./woody
 rm woody
+rm test1_no_pie
 
 gcc ./tests/test1.c -o test1
 echo "\n${_GREEN}Creating and Executing woody with normal binary...${_END}\n"
 ./woody_woodpacker test1
 ./woody
 rm woody
+rm test1
 
 # Test with bin/ls binary
 echo "\n${_GREEN}Creating and Executing woody with /bin/ls binary...${_END}\n"
@@ -29,7 +31,12 @@ rm woody
 echo "\n${_GREEN}Creating and Executing woody with /bin/echo binary...${_END}\n"
 ./woody_woodpacker /bin/echo
 ./woody "This is echo binaryyy"
-
 rm woody
-rm test1
-rm test1_no_pie
+
+# Test with file close to page size
+echo "\n${_GREEN}Creating and Executing woody with binary close to page_size...${_END}\n"
+gcc -no-pie ./tests/test_close_to_page_size.c -o test_close_to_page_size
+./woody_woodpacker test_close_to_page_size
+./woody
+rm woody
+rm test_close_to_page_size
