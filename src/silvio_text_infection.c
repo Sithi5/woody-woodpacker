afddcb04985fc962b72d5adcdef6bc58a79e74eb
Sithi5 <ma.sithis@gmail.com>
2021-08-18 13:59:59 +0200

    start creating function choose_infection_method.

--- src/silvio_text_infection.c
+++ src/silvio_text_infection.c
@@ -156,6 +156,30 @@ void overwrite_keysection_payload(t_woody *woody)
     }
 }
 
+void choose_infection_method(t_woody *woody)
+{
+    for (size_t i = 0; i < woody->ehdr->e_phnum; i++)
+    {
+        if (woody->phdr[i].p_type == PT_LOAD && woody->phdr[i].p_flags == (PF_R | PF_X))
+        {
+            //text found here, get the offset of the end of the section;
+            woody->text_start_offset = woody->phdr[i].p_offset;
+            woody->text_end_offset = woody->text_start_offset + woody->phdr[i].p_filesz;
+            woody->text_section_size = woody->phdr[i].p_filesz;
+
+            // Check if there is enought space for our payload in the text section.
+            if (woody->text_end_offset % PAGE_SIZE + woody->payload_size < PAGE_SIZE)
+            {
+                silvio_text_infection(woody);
+            }
+            else
+            {
+                error(ERROR_NOT_ENOUGHT_SPACE_FOR_PAYLOAD, woody);
+            }
+        }
+    }
+}
+
 void silvio_text_infection(t_woody *woody)
 {
     // Create the output file
