03d080d2e005e8c3c69e24d3bb7ba7b93d4bee29
Sithi5 <ma.sithis@gmail.com>
2021-08-20 10:41:53 +0200

    fix small problem with function print

--- asm/xor_cipher_32.asm
+++ asm/xor_cipher_32.asm
@@ -11,8 +11,8 @@ section .text
 
 
 asm_xor_cipher:
-    push ebp
-    mov ebp, esp
+    push ebp ; Sauvegarde de l’ancien bas de pile
+    mov ebp, esp ; on remonte le bas de pile a la position du haut de la pile
 
     mov edx, [ebp + 20] ; keylen
     dec edx ; keylen
@@ -40,49 +40,23 @@ _encrypt:
     jge _encrypt
 
 return:
-    mov esp, ebp
-    pop ebp
+    leave ; = mov esp, ebp et pop ebp => la pile reprend son ancien etat
     ret
 
-
-print_newline:
-    push ebp
-    mov ebp, esp
-
-    push eax
-    push ebx
-    push ecx
-    push edx
-
-    sub esp, 4 ; push string to write on stack
-    push 10 ;\n
-
-	mov ecx, esp        ; string to write
-    mov eax,write
-	mov ebx,STDOUT
-	mov edx, 4     ; length of string to write
-	syscall              ; call the kernel
-    add esp, 4
-
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-    mov esp , ebp
-    pop ebp
-    ret
 
 debug_print:
-    call print_newline
-    push ebp
-    mov ebp, esp
+    push ebp ; Sauvegarde de l’ancien bas de pile
+    mov ebp, esp ; on remonte le bas de pile a la position du haut de la pile
 
     push eax
     push ebx
     push ecx
     push edx
 
-    sub esp, 8 ; push string to write on stack
+    call print_newline
+
+    ; Pushing string on stack
+    sub esp, 8 ; get 8 bytes on stack to push string on it
     push 'g'
     push 'debu'
 
@@ -91,13 +65,51 @@ debug_print:
 	mov ebx,STDOUT
 	mov edx, 8     ; length of string to write
 	syscall              ; call the kernel
+
+    ; Removing string on stack to restore it.
+    pop eax
+    pop eax
     add esp, 8
+
+
+    call print_newline
 
     pop edx
     pop ecx
     pop ebx
     pop eax
-    mov esp , ebp
-    pop ebp
-    call print_newline
+
+    leave ; = mov esp, ebp et pop ebp => la pile reprend son ancien etat
+    ret
+
+print_newline:
+    push ebp ; Sauvegarde de l’ancien bas de pile
+    mov ebp, esp ; on remonte le bas de pile a la position du haut de la pile
+
+    push eax
+    push ebx
+    push ecx
+    push edx
+
+    ; Pushing string on stack
+    sub esp, 4 ; get 4 bytes on stack to push string on it
+    push 10
+
+	mov ecx, esp        ; string to write
+    mov eax,write
+	mov ebx,STDOUT
+	mov edx, 4     ; length of string to write
+	syscall              ; call the kernel
+
+    ; Removing string on stack to restore it.
+    pop eax
+    pop eax
+    add esp, 4
+
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+
+    leave ; = mov esp, ebp et pop ebp => la pile reprend son ancien etat
     ret
